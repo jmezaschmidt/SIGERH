77effceafd2b06dc475f1b643206df4d4dc15618
Mau Muñoz Chaves <mau.mchaves@gmail.com>
2012-11-07 17:14:27 -0600

    ..

--- BaseDatos/StoresProcedures/Usuarios/llenado-SP/llenado_crearContacto.sql
+++ BaseDatos/StoresProcedures/Usuarios/llenado-SP/llenado_crearContacto.sql
@@ -1,3 +1,6 @@
 exec sp_crearContactoUsuario 3452706, 'jmezaschmidt@gmail.com', 'Email'
+exec sp_crearContactoUsuario 3452706, 'jmeza@gmail.com', 'Email'
+exec sp_crearContactoUsuario 3452706, '26823456', 'Telefono'
+exec sp_crearContactoUsuario 3452706, '87965412', 'Celular'
 
 exec sp_verContactosUsuario 3452706
--- SIGERHSis/Console/Program.cs
+++ SIGERHSis/Console/Program.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using SIGERHSis;
 using SIGERHSis.LibreriaComun.ModuloExpedientes;
 using SIGERHSis.LibreriaComun.ModuloOrganizacion;
 using SIGERHSis.LibreriaComun.ModuloUsuarios;
 using SIGERHSis.Controladores.ModuloUsuarios;
-<<<<<<< HEAD
 using SIGERHSis.Controladores.ModuloExpedientes;
-=======
->>>>>>> 6f467f880a9322b753e8bbd8452fb9467e94d912
 
 namespace ConsoleApp
 {
@@ -18,27 +16,43 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             /*ControladorUsuarios cu = new ControladorUsuarios();
-=======
             ControladorUsuarios cu = new ControladorUsuarios();
->>>>>>> 6f467f880a9322b753e8bbd8452fb9467e94d912
             Usuario u = Usuario.obtenerUsuarioActual();
             u.Nickname = "javier.meza1";
             u.Password = "3452706";
             u.TipoUsuario = "Administrador";
-<<<<<<< HEAD
             Console.WriteLine(cu.autenticarUsuario());*/
 
             ControladorExpedientes ce = new ControladorExpedientes();
             Colaborador c = new Colaborador();
+            c = ce.obtenerInformacionGeneral(114740899);
+            Console.WriteLine(c.Cedula);
+            Console.WriteLine(c.Nombre);
+            Console.WriteLine(c.FechaNacimiento);
+            Console.WriteLine(c.Puesto);
+            Console.WriteLine(c.Departamento);
+            Console.WriteLine(c.FechaIngreso);
+            Console.WriteLine(c.Estado);
+            Console.WriteLine("-----------------------");
             c = ce.obtenerInformacionGeneral(3452706);
-
-            Console.WriteLine("1234");
-=======
-            //Console.WriteLine(cu.autenticarUsuario());
-            Console.WriteLine("mau2");
->>>>>>> 6f467f880a9322b753e8bbd8452fb9467e94d912
+            Console.WriteLine(c.Cedula);
+            Console.WriteLine(c.Nombre);
+            Console.WriteLine(c.FechaNacimiento);
+            Console.WriteLine(c.Puesto);
+            Console.WriteLine(c.Departamento);
+            Console.WriteLine(c.FechaIngreso);
+            Console.WriteLine(c.Estado);
+            Console.WriteLine("-----------------------");
+            DataTable dt = new DataTable();
+            dt = ce.obtenerContactos();
+            foreach(DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["Tipo Contacto"]);
+                Console.WriteLine(dr["Contacto"]);
+            }
+            
+            Console.WriteLine("mau23");
             Console.ReadLine();
         }
     }
